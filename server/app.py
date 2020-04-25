963710193b301d80bbb64355fae00aa318670497
oanadinca <oana_maria.dinca@yahoo.com>
2020-04-25 15:21:09 +0300

    refactor of servers's app.py

--- server/app.py
+++ server/app.py
@@ -5,14 +5,5 @@ import database
 
 app = Flask(__name__)
 
-#asta nu merge
-@app.route("/")
-def index():
-    return render_template(
-        "index.html",
-        home = True, history = False, products = False, add = False, dataFound = False
-    )
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
