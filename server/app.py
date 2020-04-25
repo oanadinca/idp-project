7fb546d12c9adf82d7fe6fdec7aad69ed250682c
oanadinca <oana_maria.dinca@yahoo.com>
2020-04-25 21:17:57 +0300

    added prometheus

--- server/app.py
+++ server/app.py
@@ -3,9 +3,11 @@ import random
 import socket
 import database	
 from flask_cors import CORS, cross_origin
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+metrics = PrometheusMetrics(app)
 
 if __name__ == "__main__":
 
