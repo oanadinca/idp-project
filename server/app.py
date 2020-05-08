de75e91b50b12187eb58f4de6aa285df4f2f8a59
oanadinca <oana_maria.dinca@yahoo.com>
2020-05-08 12:10:20 +0300

    yml refactor

--- server/app.py
+++ server/app.py
@@ -10,6 +10,7 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 metrics = PrometheusMetrics(app)
 metrics.info('app_info', 'Application info', version='1.0.3')
 
+
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0", port=80)
